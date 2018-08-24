62e4b9b755ed952a42322bad921ba98a27def845
Collins <collinsdelan@gmail.com>
2018-08-24 23:08:14 +0300

    Rectify docker cmd

--- deployment.sh
+++ deployment.sh
@@ -4,6 +4,6 @@ set -e
 
 docker build -t wecs/demo:$CIRCLE_SHA1 .
 
-sudo  docker push wecs/demo:$CIRCLE_SHA1
+docker push wecs/demo:$CIRCLE_SHA1
 
 # sudo kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
