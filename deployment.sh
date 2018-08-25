a9d4acc5a75057606fe2d859394b6cf7f389a598
Collins <collinsdelan@gmail.com>
2018-08-25 23:03:28 +0300

    Add k8s username as env var

--- deployment.sh
+++ deployment.sh
@@ -10,8 +10,7 @@ NAME=cd.k8s.local
 kops export kubecfg ${NAME}
 
 export PASSWORD=`kops get secrets kube --type secret -oplaintext`
-# kubectl config set-credentials cluster-admin 
 
-echo "✓"
+sudo kubectl --insecure-skip-tls-verify=true --username=$USERNAME --password=$PASSWORD --server https://api-cd-k8s-local-con0b0-610798260.us-west-2.elb.amazonaws.com set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
 
-sudo kubectl --insecure-skip-tls-verify=true --username=admin --password=$PASSWORD --server https://api-cd-k8s-local-con0b0-610798260.us-west-2.elb.amazonaws.com set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
+echo "✓ Successful..."
