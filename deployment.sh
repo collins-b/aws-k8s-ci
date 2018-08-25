57a833358b07b0bea9162e80be8b66fb3d3e6c68
Collins <collinsdelan@gmail.com>
2018-08-25 18:56:19 +0300

    Troubleshoot kops cluster

--- deployment.sh
+++ deployment.sh
@@ -7,8 +7,8 @@ docker build -t wecs/demo:$CIRCLE_SHA1 .
 docker login -u="$HUB_USER" -p="$HUB_PASS" docker.io  && docker push wecs/demo:$CIRCLE_SHA1
 export KOPS_STATE_STORE=s3://delan
 echo $KOPS_STATE_STORE
-# NAME=cd.k8s.local
-# kops export kubecfg ${NAME}
+NAME=cd.k8s.local
+kops export kubecfg ${NAME}
 # kubectl version
 
 echo "✓"
