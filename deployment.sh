27b9c26ea6a3e4263fa488c0c4950e2c8b6df625
Collins <collinsdelan@gmail.com>
2018-08-25 10:32:02 +0300

    Debug dockerhub

--- deployment.sh
+++ deployment.sh
@@ -4,6 +4,6 @@ set -e
 
 docker build -t wecs/demo:$CIRCLE_SHA1 .
 
-docker login -u="$HUB_USER" -p="$HUB_PASS" hub.docker.com  && docker push wecs/demo:$CIRCLE_SHA1
+docker login -u="$HUB_USER" -p="$HUB_PASS" index.docker.io/v1  && docker push wecs/demo:$CIRCLE_SHA1
 
 # sudo kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
