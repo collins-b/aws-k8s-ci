7e2b614b382d500e4b068717fa1c96a8e7ef9716
Collins <collinsdelan@gmail.com>
2018-08-25 13:19:51 +0300

    aws

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
 set -e
-echo $CIRCLE_SHA1
+aws --version
 docker build -t wecs/demo:$CIRCLE_SHA1 .
 
 docker login -u="$HUB_USER" -p="$HUB_PASS" docker.io  && docker push wecs/demo:$CIRCLE_SHA1
 
-# sudo kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
+sudo kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
