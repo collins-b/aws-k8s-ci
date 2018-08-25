1867e52b36d1998d7997e37a1d1ad3fc5f237b6c
Collins <collinsdelan@gmail.com>
2018-08-25 17:32:32 +0300

    Test kops

--- deployment.sh
+++ deployment.sh
@@ -5,7 +5,7 @@ aws --version
 docker build -t wecs/demo:$CIRCLE_SHA1 .
 
 docker login -u="$HUB_USER" -p="$HUB_PASS" docker.io  && docker push wecs/demo:$CIRCLE_SHA1
-
+kops export kubecfg cd.k8s.local
 kubectl version
 
 # sudo kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
