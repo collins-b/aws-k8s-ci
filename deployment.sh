abe03e86c924435d43ffaacfb3fe930009bf46ad
Collins <collinsdelan@gmail.com>
2018-08-25 10:56:36 +0300

    Tag image

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-docker build -t wecs/demo:$CIRCLE_SHA1 .
+docker build -t demo .
+
+docker tag demo wecs/demo:$CIRCLE_SHA1
 
 docker login -u="$HUB_USER" -p="$HUB_PASS" index.docker.io/v1  && docker push wecs/demo:$CIRCLE_SHA1
 
