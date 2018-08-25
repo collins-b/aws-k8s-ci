0aa060f073d163fa6cad42a2cc48be3c9b2aac3d
Collins <collinsdelan@gmail.com>
2018-08-25 17:25:26 +0300

    Test kubectl cmd

--- deployment.sh
+++ deployment.sh
@@ -6,4 +6,6 @@ docker build -t wecs/demo:$CIRCLE_SHA1 .
 
 docker login -u="$HUB_USER" -p="$HUB_PASS" docker.io  && docker push wecs/demo:$CIRCLE_SHA1
 
-sudo kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
+kubectl version
+
+# sudo kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=wecs/demo:$CIRCLE_SHA1
